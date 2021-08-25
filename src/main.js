2c7dc81f767f96ff8bcb4f97ad115f09edc5395a
mochiokun <aaaa>
2021-08-25 23:06:09 +0900

    ベーシンク認証とherokuapp名の入力に対応

--- src/main.js
+++ src/main.js
@@ -1,27 +1,67 @@
 "use strict";
 
-// サーバ接続先のURL（パスは/startNew）
-const serverUrl = 'https://nicomatsu-demo.herokuapp.com/startNew';
+// サーバ接続先のURL
+let serverUrl = 'https://nicomatsu.herokuapp.com';
 
+// 接続先URL（HerokuApp名）を入力して変更したい場合はtrue
+// 本設定をtrueにした場合、上記のserverUrlは無視される
+const hrokuAppNameChangeableFlg = true; //true or false　デフォルト：true
 
 const electron = require('electron');
 const elc_app = electron.app;
 const elc_BrowserWindow = electron.BrowserWindow;
 const ipcMain = electron.ipcMain;
 const path = require('path');
-let inputWindow;
-let loginWindow;
-let displayWindow;
-let controllerWindow
-let surveyWindow
-let isLoginWindow = false;
+let connectWindow;      // 接続先HerokuApp名の入力用のWindow
+let inputWindow;        // 接続先ルーム名の入力用のWindow
+let loginWindow;        // 認証情報の入力用のWindow
+let displayWindow;      // コメント表示用のWindow（透明化）
+let controllerWindow;   // 投稿用ページ表示用のWindow
+let surveyWindow;       // アンケートページ表示用のWindow
 
+// ログイン認証の重複発火対策
+let isLoginWindow = false;
 
 //読み込み時イベント
 elc_app.on('ready', function (event) {
+  if(hrokuAppNameChangeableFlg){
+    // HerokuApp名を入力可能にしている場合は、アプリ名入力画面を開く
+    connectWindow = new electron.BrowserWindow({
+        width: 500,
+        height: 220,
+        webPreferences: {
+          nodeIntegration: false,
+          contextIsolation: true,
+          preload: __dirname + '/preload.js'
+        }
+    });
+    // 前面に表示
+    connectWindow.setAlwaysOnTop(true);
+    connectWindow.loadURL('file://' + __dirname + '/connect.html');
+  }else{
+    // URL固定の場合はルーム名入力画面を開く
+    inputWindow = new electron.BrowserWindow({
+        width: 500,
+        height: 220,
+        webPreferences: {
+          nodeIntegration: false,
+          contextIsolation: true,
+          preload: __dirname + '/preload.js'
+        }
+    });
+    // 前面に表示
+    inputWindow.setAlwaysOnTop(true);
+    // ルーム名入力前にプロキシのログイン要求を発生させたいので、サーバアクセスしてstartNewページを取得
+    inputWindow.loadURL(serverUrl+'/startNew');
+  }
+});
+
+//接続先入力時のイベント
+ipcMain.on("heroku-app-name", (event, herokuAppName)=>{
+  serverUrl = 'https://' + herokuAppName + '.herokuapp.com/startNew';
   inputWindow = new electron.BrowserWindow({
       width: 500,
-      height: 200,
+      height: 220,
       webPreferences: {
         nodeIntegration: false,
         contextIsolation: true,
@@ -33,50 +73,55 @@ elc_app.on('ready', function (event) {
 
   // ルーム名入力前にプロキシのログイン要求を発生させたいので、サーバアクセスしてstartNewページを取得
   inputWindow.loadURL(serverUrl);
+  // 接続先入力ウィンドウを閉じる
+  connectWindow.close();
 });
+
 
 
 // ログイン要求時に発火するイベント
 elc_app.on("login", (event, webContents, request, authInfo, callback)=>{
-    // プロキシサーバーからの要求だったら続行
-    if(authInfo.isProxy){
-        // 重複発火対策
-        if(!isLoginWindow){
-            isLoginWindow = true;
-        }
-        else{
-            return;
-        }
-        // 認証情報が送信されるまで待機
-        event.preventDefault();
-        loginWindow = new electron.BrowserWindow({
-            width: 500,
-            height: 200,
-            resizable: false,
-            webPreferences: {
-            nodeIntegration: false,
-            contextIsolation: true,
-            preload: __dirname + '/preload.js'
-            }
-        });
-
-        // 前面に表示
-        loginWindow.setAlwaysOnTop(true);
-
-        // ログイン情報の入力ウィンドウをロード
-        loginWindow.setMenu(null);
-        loginWindow.loadURL('file://' + __dirname + '/login.html');
-
-        // IPCチャネル"proxy-auth"で受信待機
-        ipcMain.once("proxy-auth", (event, username, password)=>{
-
-            // 受信した認証情報をプロキシサーバーへ転送
-            callback(username, password);
-            isLoginWindow = false;
-            // ログインウィンドウを閉じる
-            loginWindow.close();
-        });
+    // 重複発火対策
+    if(!isLoginWindow){
+        isLoginWindow = true;
     }
+    else{
+        return;
+    }
+    // 認証情報が送信されるまで待機
+    event.preventDefault();
+    loginWindow = new electron.BrowserWindow({
+        width: 500,
+        height: 220,
+        resizable: false,
+        webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: true,
+        preload: __dirname + '/preload.js'
+        }
+    });
+
+    // 前面に表示
+    loginWindow.setAlwaysOnTop(true);
+
+    // ログイン情報の入力ウィンドウをロード
+    loginWindow.setMenu(null);
+    // Proxy認証なのかコメント表示用のBasic認証がわかるようにログイン画面を出し分け
+    if(authInfo.isProxy){
+      loginWindow.loadURL('file://' + __dirname + '/login.html');
+    }else{
+      loginWindow.loadURL('file://' + __dirname + '/basicAuth.html');
+    }
+
+    // IPCチャネル"proxy-auth"で受信待機
+    ipcMain.once("proxy-auth", (event, username, password)=>{
+
+        // 受信した認証情報をプロキシサーバーへ転送
+        callback(username, password);
+        isLoginWindow = false;
+        // ログインウィンドウを閉じる
+        loginWindow.close();
+    });
 });
 
 //レンダラプロセスでルーム名が送信されたら動作
